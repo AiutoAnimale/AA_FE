b01a4f6c34644007bdbafefdb7b62b3b26122b21
downpour07 <choiheeyoung10@gmail.com>
2024-09-25 20:44:53 +0900

    ⚡️ :: navigation 추가

--- aa/src/components/Header.jsx
+++ aa/src/components/Header.jsx
@@ -2,20 +2,29 @@ import { useState } from "react";
 import { breakpoints } from "../style/device";
 import { color } from "../style/theme";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as LogoSVG } from "../assets/Logo.svg";
 
 export default function Header() {
+    const navigate = useNavigate();
+    const [isLogin, setIsLogin] = useState();
 
     return (
         <>
             <Container>
                 <List>
-                    <TextLogo />
-                    <Data>실종</Data>
-                    <Data>커뮤니티</Data>
-                    <Data>지식공유</Data>
+                    <DataContainer>
+                        <TextLogo onClick={() => navigate("/main")} />
+                        <Data onClick={() => navigate("/missing")}>실종</Data>
+                        <Data onClick={() => navigate("/community")}>커뮤니티</Data>
+                        <Data onClick={() => navigate("/knowing")}>지식톡톡</Data>
+                    </DataContainer>
+                    {isLogin ? (
+                        <Data onClick={() => navigate("/info")}>님</Data>
+                    ) : (
+                        <Btn onClick={() => navigate("/login")}>로그인하기</Btn>
+                    )}
                 </List>
-                <Btn>로그인하기</Btn>
             </Container>
         </>
     );
@@ -25,11 +34,18 @@ const Container = styled.header`
     padding: 15px 0;
     display: flex;
     align-items: center;
-    justify-content: space-evenly;
+    justify-content: center;
     background-color: ${color.White};
 `;
 
 const List = styled.div`
+    width: 885px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const DataContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -39,14 +55,15 @@ const List = styled.div`
     }
     @media screen and (max-width: ${breakpoints.tabletSmall}) {
         gap: 20px;
-   }
-   @media screen and (max-width: ${breakpoints.mobileSmall}) {
+    }
+    @media screen and (max-width: ${breakpoints.mobileSmall}) {
         gap: 10px;
-   }
+    }
 `
 
 const Data = styled.button`
     background-color: ${color.White};
+    color: ${color.Black};
     border: none;
     font-size: 1rem;
 
@@ -61,6 +78,7 @@ const Btn = styled.button`
     border: solid 1px ${color.Gray[2]};
     font-size: 0.8rem;
     font-weight: bolder;
+    color: ${color.Black};
     border-radius: 5px;
 
     &:hover {
