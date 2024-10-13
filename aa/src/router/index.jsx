115b07e829ec945ad1c34c22c0ea01ca7d305252
downpour07 <choiheeyoung10@gmail.com>
2024-10-13 20:16:32 +0900

    📝 :: router 수정

--- aa/src/router/index.jsx
+++ aa/src/router/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
 import Login from "../pages/Auth";
-import Signup from "../pages/User";
+import UserData from "../pages/User/UserData";
+import UserNickName from "../pages/User/UserNickName";
+import UserPetData from "../pages/User/UserPetData";
 import Community from "../pages/Community";
 import Main from "../pages/Main";
 import Missing from "../pages/Missing";
@@ -13,9 +15,11 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Layout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signupData" element={<UserData />} />
+          <Route path="/signupNick" element={<UserNickName />} />
+          <Route path="/signupPet" element={<UserPetData />} />
           <Route path="/main" element={<Main />} />
           <Route path="/missing" element={<Missing />} />
           <Route path="/community" element={<Community />} />
