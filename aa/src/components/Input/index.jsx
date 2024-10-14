c30e9c780d82272888b505c6f9fda58878e45917
downpour07 <choiheeyoung10@gmail.com>
2024-10-14 19:42:39 +0900

    📝 :: Input Component 수정

--- aa/src/components/Input/index.jsx
+++ aa/src/components/Input/index.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 import { breakpoints } from "../../style/device";
 import { color } from "../../style/theme";
 import styled from "styled-components";
+import { ReactComponent as ViewPwSVG } from "../../assets/icon/ViewPw.svg";
 
 export default function Input(props) {
 
     return (
         <Container>
             <Text>{props.text}</Text>
-            <InputContainer placeholder={props.placeholder} type={props.type} />
+            {props.state == 'password' ? (
+                <PwConatiner>
+                    <InputPassword placeholder={props.placeholder} type={props.type} />
+                    <ViewCotainer onClick={props.onClick}>
+                      <ViewPw />
+                    </ViewCotainer>
+                </PwConatiner>
+            ) : (
+                <InputContainer placeholder={props.placeholder} type={props.type} />
+            )}
             <ErrorText>{props.error}</ErrorText>
         </Container>
     );
@@ -58,3 +68,54 @@ const InputContainer = styled.input`
         border: solid 1px ${color.Orange[1]};
     }
 `;
+
+const PwConatiner = styled.div`
+    width: 100%;
+    height: 50%;
+    border: solid 1px ${color.Gray[1]};
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+        cursor: pointer;
+    }
+    &:focus {
+        outline: none;
+        border: solid 1px ${color.Orange[1]};
+    }
+`
+
+const InputPassword = styled.input`
+    width: 80%;
+    height: 95%;
+    border-radius: 5px;
+    padding: 0 3%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 1rem;
+    font-weight: medium;
+    border: none;
+
+    &:focus {
+        outline: none;
+    }
+`
+
+const ViewCotainer = styled.button`
+    width: 14%;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    border: none;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+const ViewPw = styled(ViewPwSVG)``;
