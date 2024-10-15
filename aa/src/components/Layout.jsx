2b560ff638cbe4c3d04b25604eb7e0a8026034f8
downpour07 <choiheeyoung10@gmail.com>
2024-10-15 22:28:41 +0900

    📝 :: Layout 수정

--- aa/src/components/Layout.jsx
+++ aa/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import Header from "./Header";
-import Footer from './Footer';
 import styled from "styled-components";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout() {
     return (
