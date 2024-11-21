d581346a88ac6e0560c71168e78edccac9d7f8ef
downpour07 <choiheeyoung10@gmail.com>
2024-11-21 22:03:22 +0900

    📝 :: router 수정

--- aa/src/router/index.jsx
+++ aa/src/router/index.jsx
@@ -5,11 +5,7 @@ import Login from "../pages/Auth";
 import UserData from "../pages/User/UserData";
 import UserNickName from "../pages/User/UserNickName";
 import UserPetData from "../pages/User/UserPetData";
-import Community from "../pages/Community";
 import Main from "../pages/Main";
-import Missing from "../pages/Missing";
-import Knowing from "../pages/Knowing";
-import Info from "../pages/Info";
 
 export default function Router() {
   return (
@@ -19,12 +15,8 @@ export default function Router() {
         <Route path="/signupData" element={<UserData />} />
         <Route path="/signupNick" element={<UserNickName />} />
         <Route path="/signupPet" element={<UserPetData />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="/main" element={<Main />} />
-          <Route path="/missing" element={<Missing />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/knowing" element={<Knowing />} />
-          <Route path="/info" element={<Info />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
         </Route>
       </Routes>
     </BrowserRouter>
