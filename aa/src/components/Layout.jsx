08e69c9f8b8a267eb0a23d7cbcca39634b1f91b5
timdind <no3232164@dsm.hs.kr>
2024-11-22 20:32:27 +0900

    fix:: Layout 되돌리기

--- aa/src/components/Layout.jsx
+++ aa/src/components/Layout.jsx
@@ -1,17 +1,14 @@
 import { Outlet, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Header";
-import CommunityHeader from "./CommunityHeader";
 import Footer from "./Footer";
 
 export default function Layout() {
   const location = useLocation();
 
-  const isCommunityPage = location.pathname.startsWith("/community");
-
   return (
     <Container>
-      {isCommunityPage ? <CommunityHeader /> : <Header />}
+      <Header />
       <Outlet />
       <Footer />
     </Container>
