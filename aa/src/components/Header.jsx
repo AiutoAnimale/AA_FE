cb1f853437fb6840030093ebe53e5f6c7e293f16
timdind <no3232164@dsm.hs.kr>
2024-11-22 19:47:14 +0900

    fix:: Header 실종, 커뮤니티 => 커뮤니티, 실종
    Design따라서 Header 순서 수정함

--- aa/src/components/Header.jsx
+++ aa/src/components/Header.jsx
@@ -6,95 +6,97 @@ import { useNavigate } from "react-router-dom";
 import { ReactComponent as LogoSVG } from "../assets/Logo.svg";
 
 export default function Header() {
-    const navigate = useNavigate();
-    const [isLogin, setIsLogin] = useState();
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState();
 
-    const MainList = [
-        { name: "실종", path: "/missing" },
-        { name: "커뮤니티", path: "/community" },
-     ];
+  const MainList = [
+    { name: "커뮤니티", path: "/community" },
+    { name: "실종", path: "/missing" },
+  ];
 
-    return (
-        <>
-            <Container>
-                <List>
-                    <DataContainer>
-                        <TextLogo onClick={() => navigate("/main")} />
-                        {MainList.map((item, index) => (
-                            <Data key={index} onClick={() => navigate(item.path)}>{item.name}</Data>
-                        ))}
-                    </DataContainer>
-                    {isLogin ? (
-                        <Data onClick={() => navigate("/info")}>님</Data>
-                    ) : (
-                        <Btn onClick={() => navigate("/login")}>로그인하기</Btn>
-                    )}
-                </List>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <List>
+          <DataContainer>
+            <TextLogo onClick={() => navigate("/main")} />
+            {MainList.map((item, index) => (
+              <Data key={index} onClick={() => navigate(item.path)}>
+                {item.name}
+              </Data>
+            ))}
+          </DataContainer>
+          {isLogin ? (
+            <Data onClick={() => navigate("/info")}>님</Data>
+          ) : (
+            <Btn onClick={() => navigate("/login")}>로그인하기</Btn>
+          )}
+        </List>
+      </Container>
+    </>
+  );
 }
 
 const Container = styled.header`
-    padding: 15px 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: ${color.White};
+  padding: 15px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${color.White};
 `;
 
 const List = styled.div`
-    max-width: 885px;
-    padding: 0px 30px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-`
+  max-width: 885px;
+  padding: 0px 30px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const DataContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    @media screen and (min-width: ${breakpoints.tabletLarge}) {
-        gap: 30px;
-    }
-    @media screen and (max-width: ${breakpoints.tabletSmall}) {
-        gap: 20px;
-    }
-    @media screen and (max-width: ${breakpoints.mobileSmall}) {
-        gap: 10px;
-    }
-`
+  @media screen and (min-width: ${breakpoints.tabletLarge}) {
+    gap: 30px;
+  }
+  @media screen and (max-width: ${breakpoints.tabletSmall}) {
+    gap: 20px;
+  }
+  @media screen and (max-width: ${breakpoints.mobileSmall}) {
+    gap: 10px;
+  }
+`;
 
 const Data = styled.button`
-    background-color: ${color.White};
-    color: ${color.Gray[8]};
-    border: none;
-    font-size: 1rem;
+  background-color: ${color.White};
+  color: ${color.Gray[8]};
+  border: none;
+  font-size: 1rem;
 
-    &:hover {
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+  }
 
-    @media screen and (max-width: ${breakpoints.mobileSmall}) {
-        display: none;
-    }
-`
+  @media screen and (max-width: ${breakpoints.mobileSmall}) {
+    display: none;
+  }
+`;
 
 const Btn = styled.button`
-    padding: 6px 16px;
-    background-color: ${color.White};
-    border: solid 1px ${color.Gray[2]};
-    font-size: 0.8rem;
-    font-weight: bolder;
-    color: ${color.Black};
-    border-radius: 5px;
+  padding: 6px 16px;
+  background-color: ${color.White};
+  border: solid 1px ${color.Gray[2]};
+  font-size: 0.8rem;
+  font-weight: bolder;
+  color: ${color.Black};
+  border-radius: 5px;
 
-    &:hover {
-        cursor: pointer;
-    }
-`
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const TextLogo = styled(LogoSVG)``;
