6d0396c874825b2b0fbfc5e031da5daf2c9ff251
timdind <no3232164@dsm.hs.kr>
2024-11-22 20:10:05 +0900

    fix:: Layout에 CommunityHeader추가

--- aa/src/components/Layout.jsx
+++ aa/src/components/Layout.jsx
@@ -1,16 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Header";
+import CommunityHeader from "./CommunityHeader";
 import Footer from "./Footer";
 
 export default function Layout() {
+  const location = useLocation();
+
+  const isCommunityPage = location.pathname.startsWith("/community");
+
   return (
     <Container>
-      <Header />
+      {isCommunityPage ? <CommunityHeader /> : <Header />}
       <Outlet />
       <Footer />
     </Container>
   );
-};
+}
 
 const Container = styled.div``;
