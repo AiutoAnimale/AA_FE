c628b8beeec0b6e620a03bef0b518273918daeed
timdind <no3232164@dsm.hs.kr>
2024-11-23 21:05:39 +0900

    add:: communitymain publishing 끝

---
+++ aa/src/pages/Community/Main/index.jsx
@@ -0,0 +1,88 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { color } from "../../../style/theme";
+import DataContainer from "../../Main/DataContainer/index";
+import PopularDataContainer from "../../Main/PopularDataContainer";
+
+export default function CommunityMain() {
+  const navigate = useNavigate();
+
+  const List = [
+    {
+      text: "우리 푕힁이가 대덕SW고 대마냥이랑 만나서 싸움 ㄷㄷ",
+      number: "1",
+      tag: "일상",
+      user: "초코 보호자",
+      count: "13",
+    },
+    {
+      text: "우리 푕힁이가 대덕SW고 대마냥이랑 만나서 싸움 ㄷㄷ",
+      number: "2",
+      tag: "일상",
+      user: "초코 보호자",
+      count: "13",
+    },
+    {
+      text: "우리 푕힁이가 대덕SW고 대마냥이랑 만나서 싸움 ㄷㄷ",
+      number: "3",
+      tag: "일상",
+      user: "초코 보호자",
+      count: "13",
+    },
+    {
+      text: "우리 푕힁이가 대덕SW고 대마냥이랑 만나서 싸움 ㄷㄷ",
+      number: "4",
+      tag: "일상",
+      user: "초코 보호자",
+      count: "13",
+    },
+    {
+      text: "우리 푕힁이가 대덕SW고 대마냥이랑 만나서 싸움 ㄷㄷ",
+      number: "5",
+      tag: "일상",
+      user: "초코 보호자",
+      count: "13",
+    },
+  ];
+
+  const PopularList = [
+    {
+      text: "우리 푕힁이가 대덕SW고 대마냥이랑 만나서 싸움 ㄷㄷ",
+      tag: "일상",
+      user: "초코 보호자",
+      count: "13",
+      textContent:
+        "퍼어어어어어어어ㅓ어어어어어어어ㅓ어어어어어어어어어어ㅓ어어어어어엉",
+    },
+  ];
+
+  return (
+    <>
+      <Container>
+        <PopularDataContainer
+          data={PopularList}
+          text="인기글"
+          color={color.Orange[1]}
+          btnText="자세히 보기"
+        />
+        <DataContainer
+          data={List}
+          text="커뮤니티"
+          color={color.Orange[1]}
+          subText="서로의 이야기를 나누어봐요"
+        />
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  row-gap: 20px;
+  padding-bottom: 80px;
+  background-color: ${color.White};
+`;
