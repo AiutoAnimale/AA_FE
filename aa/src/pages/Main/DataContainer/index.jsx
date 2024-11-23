ae270a867dfb159a91a9618fc69cc41253056494
timdind <no3232164@dsm.hs.kr>
2024-11-23 19:39:08 +0900

    fix:: datacontainer에 subText추가

--- aa/src/pages/Main/DataContainer/index.jsx
+++ aa/src/pages/Main/DataContainer/index.jsx
@@ -8,93 +8,109 @@ import Tag from "../../../components/Tag";
 import MainText from "../../../components/MainText";
 
 export default function DataContainer(props) {
-
-    return (
-                <Div>
-                    <TextDiv>
-                        <Text>{props.text}</Text>
-                        <MoreBtn onClick={props.event}>{props.btnText}</MoreBtn>
-                    </TextDiv>
-                    <Bottom>
-                        {props.data.map((item, index) => (
-                            <BottomList>
-                                <Number>{item.number}</Number>
-                                <Column>
-                                    <Tag type={'tag'} backColor={color.Orange[0]} color={color.Orange[3]} data={`#${item.tag}`} />
-                                    <MainText data={item.text} size={'18px'} />
-                                    <UserName data={item.user} />
-                                </Column>
-                            </BottomList>
-                        ))}
-                    </Bottom>
-                </Div>
-    );
+  return (
+    <Div>
+      <TextDiv>
+        <Text>{props.text}</Text>
+        {props.subText && <SubText>{props.subText}</SubText>}{" "}
+        <MoreBtn onClick={props.event}>{props.btnText}</MoreBtn>
+      </TextDiv>
+      <Bottom>
+        {props.data.map((item, index) => (
+          <BottomList key={index}>
+            <Number>{item.number}</Number>
+            <Column>
+              <Tag
+                type={"tag"}
+                backColor={color.Orange[0]}
+                color={color.Orange[3]}
+                data={`#${item.tag}`}
+              />
+              <MainText data={item.text} size={"18px"} />
+              <UserName data={item.user} />
+            </Column>
+          </BottomList>
+        ))}
+      </Bottom>
+    </Div>
+  );
 }
 
 const Div = styled.div`
-    max-width: 920px;
-    width: 90%;
-    display: flex;
-    flex-direction: column;
-    row-gap: 15px;
-`
+  max-width: 920px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+`;
 
 const Text = styled.div`
-    font-size: 22px;
-    font-weight: bolder;
-    color: ${color.Black};
-    user-select: none;
-`
+  font-size: 22px;
+  font-weight: bolder;
+  color: ${color.Black};
+  user-select: none;
+`;
+
+const SubText = styled.div`
+  font-size: 12px;
+  font-weight: bold;
+  color: ${color.Gray[5]};
+  margin-top: 10px;
+  user-select: none;
+`;
 
 const TextDiv = styled.div`
-    height: 94px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: solid 1px ${color.Gray[1]};
-`
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  border-bottom: solid 1px ${color.Gray[1]};
+  width: 100%;
+  padding-bottom: 10px;
+`;
 
 const Bottom = styled.div`
-    width: 100%;
-    height: 630px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  width: 100%;
+  height: 630px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const BottomList = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 20%;
-    gap: 30px;
-`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  gap: 30px;
+`;
 
 const MoreBtn = styled.button`
-    background-color: ${color.White};
-    border: none;
-    font-size: 18px;
-    font-weight: medium;
-    color: ${color.Gray[7]};
-    user-select: none;
+  background-color: ${color.White};
+  border: none;
+  font-size: 18px;
+  font-weight: medium;
+  color: ${color.Gray[7]};
+  user-select: none;
 
-    &:hover {
-        cursor: pointer;
-    }
-`
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const Number = styled.div`
-    font-size: 24px;
-    font-weight: medium;
-    color: ${color.Orange[4]};
-    user-select: none;
-`
+  font-size: 24px;
+  font-weight: medium;
+  color: ${color.Orange[4]};
+  user-select: none;
+`;
 
 const Column = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    width: 100%;
-    row-gap: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+  row-gap: 10px;
+`;
