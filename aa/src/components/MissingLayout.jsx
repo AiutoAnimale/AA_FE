9c07540396eb00b0a08b3513b321c58bd84c23c2
timdind <no3232164@dsm.hs.kr>
2024-11-25 12:25:52 +0900

    fix:: MissingLayout수정

--- aa/src/components/MissingLayout.jsx
+++ aa/src/components/MissingLayout.jsx
@@ -1,14 +1,13 @@
 import { Outlet, useLocation } from "react-router-dom";
 import styled from "styled-components";
-import Header from "./Header";
-import Footer from "./Footer";
 import MissingHeader from "../components/MissingHeader";
 
 export default function MissingLayout() {
   const location = useLocation();
+
   return (
     <Container>
-      <MissingHeader />
+      {location.pathname !== "/missingview" && <MissingHeader />}
       <Outlet />
     </Container>
   );
