b0931f1e927eb9b3f034a30465289b2b6338bdbe
timdind <no3232164@dsm.hs.kr>
2024-11-25 09:17:55 +0900

    add:: MissingHeader 추가

---
+++ aa/src/components/MissingHeader.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { breakpoints } from "../style/device";
+import { color } from "../style/theme";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as LogoSVG } from "../assets/Logo.svg";
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  const MainList = [
+    { name: "커뮤니티", path: "/community" },
+    { name: "실종", path: "/missing" },
+  ];
+
+  return (
+    <>
+      <Container>
+        <List>
+          <DataContainer>
+            <TextLogo onClick={() => navigate("/main")} />
+            {MainList.map((item, index) => (
+              <Data
+                key={index}
+                onClick={() => navigate(item.path)}
+                isMissing={item.name === "실종"}
+              >
+                {item.name}
+              </Data>
+            ))}
+          </DataContainer>
+          <CreatePostButton onClick={() => navigate("/createpost")}>
+            커뮤니티 글 작성하기
+          </CreatePostButton>
+        </List>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.header`
+  padding: 15px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${color.White};
+`;
+
+const List = styled.div`
+  max-width: 885px;
+  padding: 0px 30px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const DataContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (min-width: ${breakpoints.tabletLarge}) {
+    gap: 30px;
+  }
+  @media screen and (max-width: ${breakpoints.tabletSmall}) {
+    gap: 20px;
+  }
+  @media screen and (max-width: ${breakpoints.mobileSmall}) {
+    gap: 10px;
+  }
+`;
+
+const Data = styled.button`
+  background-color: ${color.White};
+  color: ${(props) => (props.isMissing ? "#FF823F" : color.Gray[8])};
+  border: none;
+  font-size: 1rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: ${breakpoints.mobileSmall}) {
+    display: none;
+  }
+`;
+
+const CreatePostButton = styled.button`
+  padding: 6px 16px;
+  background-color: #ff823f;
+  color: white;
+  border: none;
+  font-size: 0.8rem;
+  font-weight: bolder;
+  border-radius: 5px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const TextLogo = styled(LogoSVG)``;
